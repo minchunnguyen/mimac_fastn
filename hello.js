9bfdf1208266cf8250a9755c16aba3e5d592bbe7
NGUYEN <minchun_nguyen@yahoo.com>
2018-04-30 14:06:37 +0200

    ajout fichier hello.js

---
+++ hello.js
@@ -0,0 +1,20 @@
+/*
+les fonctions à tester 
+
+*/
+
+export function carre(n){
+	return n*n;
+}
+
+export function test2(id){
+
+	if(id > 5){
+		var userName = prompt('Entrez votre prénom :');
+		alert(userName);
+	}else{
+		alert('saisir un nombre supérieur à 5');
+	}
+}
+
+
